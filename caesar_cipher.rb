20786aca572e7bee0b0d47354d7fd6edd1ef6a21
Samet ÇİLLİ <samet.cilli@triodor.eu>
2018-07-25 09:29:36 +0300

    şifreleme sola yapılıyordu sağa yapılması şeklinde değiştirildi.

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,20 +1,22 @@
-def convert(char, left)
-    new_i = (char.ord - left)
+def convert(char, right)
+    new_i = (char.ord + right)
     lowerlist = ("a".ord.."z".ord).to_a
     upperlist = ("A".ord.."Z".ord).to_a
+    #p upperlist
+    #p char.ord, new_i, upperlist[2], "Z".ord - new_i, upperlist["Z".ord - new_i].chr
     case char.ord
     when "a".ord.."z".ord
-        lowerlist[new_i - "a".ord].chr
+        lowerlist[(new_i - "z".ord) - 1].chr
     when "A".ord.."Z".ord
-        upperlist[new_i - "A".ord].chr
+        upperlist[(new_i - "Z".ord) - 1].chr
     else 
         char
     end
 end
-def caesar_cipher(input, left)
+def caesar_cipher(input, right)
     words = input.split("")
-    words.map!{|s| convert(s, left) }
+    words.map!{|s| convert(s, right) }
     words.join()
 end
 
-caesar_cipher('Samet CILLI', 2)
+puts caesar_cipher("What a string!", 5)
